99d0fb9a3463718f23fd0833a9c1ebc6274f6e8b
Jose Cruz <josedanielcrz@gmail.com>
2021-11-30 12:10:07 -0500

    MM-4 Create register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,22 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+{!! Html::script('css/users/register.css') !!}
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
-
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    {!! Form::open(['route' => 'register']) !!}
                         @csrf
-
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
+                            {!! Form::label('name', '{{ __('Nombre') }}', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
+                                {!! Form::text('name', '{{ old('name') }}', ['class' => 'form-control @error('name') is-invalid @enderror', 'required' => 'required']) !!}
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -24,13 +21,21 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
+                            {!! Form::label('lastname', '{{ __('Apellido') }}', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                                {!! Form::text('lastname', '{{ old('lastname') }}', ['class' => 'form-control @error('lastname') is-invalid @enderror', 'required' => 'required']) !!}
+                                @error('lastname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            {!! Form::label('email', '{{ __('Correo Electrónico') }}', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
+                            <div class="col-md-6">
+                                {!! Form::email('email', '{{ old('email') }}', ['class' => 'form-control @error('email') is-invalid @enderror', 'required' => 'required']) !!}
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -38,13 +43,10 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
+                            {!! Form::label('password', '{{ __('Contraseña') }}', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                                {!! Form::password('password', '{{ old('password') }}', ['class' => 'form-control @error('password') is-invalid @enderror', 'required' => 'required']) !!}
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -52,23 +54,18 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
+                            {!! Form::label('password-confirm', '{{ __('Nombre') }}', ['class' => 'col-md-4 col-form-label text-md-right']) !!}
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                {!! Form::password('password-confirm', null, ['class' => 'form-control', 'required' => 'required']) !!}
                             </div>
                         </div>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                                {!! Form::submit('{{ __('Register') }}', ['class' => 'btn btn-primary']) !!}
                             </div>
                         </div>
-                    </form>
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
